249d3d260cd26d17662f8233b79c23c0e9fe7f8c
Nick Kaye <nick.c.kaye@gmail.com>
2011-12-04 06:02:03 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="css/index.css" type="text/css" media="all" />
+        <title></title>
+    </head>
+    <body>
+<?php
+
+include_once("protected/config/*.php");
+
+include_once("protected/models/*.php");
+include_once("protected/controllers/*.php");
+include_once("protected/views/*.php");
+
+
+
+?>
+    </body>
+</html>
---
+++ protected/controllers/generateAdjancency.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Description of eulerWalk
+ *
+ * @author nickkaye
+ */
+class generateAdjancency{
+    //put your code here
+}
+
+?>
